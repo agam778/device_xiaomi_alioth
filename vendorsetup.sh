f46618277fc539be83368aa6c92015c3f62da92f
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2023-05-10 15:25:57 +0530

    Add mmap issues patch in vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,12 @@ else
 	git fetch https://github.com/LineageOS/android_frameworks_native/ c73885dcba344f2640d7faf1e375c03e7c5ffacc && git cherry-pick FETCH_HEAD
 fi
 cd ../..
+
+echo "Applying patch for mmap issues on linux 6.1+"
+cd art || exit 1
+if git log -150 --oneline --decorate | grep "libartbase: mem_map: Retry up to 512 times on mmap() failure"; then
+	echo "Fix was already applied"
+else
+	git fetch https://github.com/Linux4/android_art/ 67006f22e3f249587dc25e23013ba008e56438a9 && git cherry-pick FETCH_HEAD
+fi
+cd ..
